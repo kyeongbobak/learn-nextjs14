648975e5387c7aaa1b5a6d6fcee09b2875a38417
kyeongbobak <kyeong0914@gmail.com>
2024-05-20 14:37:58 +0900

    promise all 추가

--- app/(movies)/movies/[id]/page.jsx
+++ app/(movies)/movies/[id]/page.jsx
@@ -1,22 +1,31 @@
 import { API_URL } from "../../../(home)/page";
 
-// await new Promise((resolve) => setTimeout(resolve, 5000));
 async function getMovies(id) {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  console.log(`Fetching movies : ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   const res = await fetch(`${API_URL}/${id}`);
+  return res.json();
+}
+
+async function getVideos(id) {
+  console.log(`Fetching videos : ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const res = await fetch(`${API_URL}/${id}/videos`);
   return res.json();
 }
 
 export default async function MoviesDetail(props) {
   const { params, searchParams } = props;
-
-  const movie = await getMovies(params.id);
+  console.log("===============");
+  console.log("start fetching");
+  const [movie, videos] = await Promise.all([getMovies(params.id), getVideos(params.id)]);
+  console.log("end fetching");
   console.log(params);
   console.log(searchParams);
 
   return (
     <>
-      <h1>{movie.title}</h1>
+      <h1>{movie.title}</h1>{" "}
     </>
   );
 }
